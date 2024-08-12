a1332f74676213add8072b98eeca8907ae4c4a60
Kartik-720 <kartiksharma86600@gmail.com>
2024-08-12 19:19:29 +0530

    Initial Commit

---
+++ Socialize/SocializeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocializeApp.swift
+//  Socialize
+//
+//  Created by Kartik Sharma on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SocializeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
